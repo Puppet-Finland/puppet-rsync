ac264eb96b2b4adb52236dda73054aea7e3f5202
Mikko Vilpponen <vilpponen@protecomp.fi>
2019-04-08 12:16:12 +0300

    Add rrsync class to extract rrsync script in place

---
+++ manifests/rrsync.pp
@@ -0,0 +1,15 @@
+# Copy rrsync script to local binary dir
+#
+# @example
+#   include rsync::rrsync
+class rsync::rrsync {
+  $rrsync_location = '/usr/share/doc/rsync/scripts/rrsync.gz'
+  $rrsync_target_dir = '/usr/local/bin'
+
+  exec { 'rsync-copy-rrsync':
+    user    => 'root',
+    group   => 'root',
+    command => "/bin/cp ${rrsync_location} ${rrsync_target_dir} && /bin/gunzip ${rrsync_target_dir}/rrsync.gz",
+    creates => "${rrsync_target_dir}/rrsync",
+  }
+}
